c9a7ceafcda38e35eb35e77ff8bd67447829f4e6
Aviral Singh <aviralsingh@protonmail.com>
2020-09-25 12:41:18 +0530

    Move title_screen func and show_text from main to ui

--- main.py
+++ main.py
@@ -37,74 +37,13 @@ class Game():
         # Set Block
         # Block Randomiser
 
-    # UI
-    '''def ui(self):
-        # Separator Lines
-        # Separator 1
-        separator1_pos_x = int(2 * self.display_width/3)
-        separator1_length = self.display_height
-
-        pygame.draw.rect(
-            self.display, self.color.white,
-            (separator1_pos_x, 0, 2, separator1_length))
-
-        separator2_pos_x = separator1_pos_x
-        separator2_pos_y = int(self.display_height/2)
-        separator2_width = int(self.display_width - separator1_pos_x)
-
-        pygame.draw.rect(
-            self.display,
-            self.color.white,
-            (separator2_pos_x,
-             separator2_pos_y,
-             separator2_width,
-             2))'''
-
-        # Score
-        # Update with every block set
-
-        # Next Block Display
-
-        # Title Screen and you Lose Screen
-        # Text Display
         # Check For Events
-
-    def title_screen(self):
-        title_text = 'Welcome To Tetris'
-        color = self.color.green
-
-        self.show_text(
-            title_text,
-            color,
-            self.display_width / 2,
-            self.display_height / 2 - 100,
-            font_size=40)
-        title_sub_text = 'Press P To Play Or Q To Exit'
-
-        self.show_text(
-            title_sub_text,
-            color,
-            self.display_width / 2,
-            self.display_height / 2,
-            font_size=20)
-
-    def show_text(self, text, color, pos_x, pos_y, font_size=40):
-        font = pygame.font.SysFont('Source Code Pro', font_size)
-        font_surface = font.render(text, 0, color)
-
-        font_width = font_surface.get_rect().width
-        font_height = font_surface.get_rect().height
-
-        self.display.blit(
-            font_surface,
-            (pos_x - font_width / 2,
-             pos_y - font_height / 2))
 
     def main(self):
         # Check for events
         while True:
-            self.ui.game_ui()
-            #self.title_screen()
+            # self.ui.game_ui()
+            self.ui.title_screen()
             self.check_events()
             pygame.display.update()
 
--- ui.py
+++ ui.py
@@ -37,4 +37,34 @@ class UI():
         # Next Block Display
 
         # Title Screen and you Lose Screen
+    def title_screen(self):
+        title_text = 'Welcome To Tetris'
+        color = [0, 255, 0]
+
+        self.show_text(
+            title_text,
+            color,
+            self.display_width / 2,
+            self.display_height / 2 - 100,
+            font_size=40)
+        title_sub_text = 'Press P To Play Or Q To Exit'
+
+        self.show_text(
+            title_sub_text,
+            color,
+            self.display_width / 2,
+            self.display_height / 2,
+            font_size=20)
+
+    def show_text(self, text, color, pos_x, pos_y, font_size=40):
+        font = pygame.font.SysFont('Source Code Pro', font_size)
+        font_surface = font.render(text, 0, color)
+
+        font_width = font_surface.get_rect().width
+        font_height = font_surface.get_rect().height
+
+        self.display.blit(
+            font_surface,
+            (pos_x - font_width / 2,
+             pos_y - font_height / 2))
         # Text Display
